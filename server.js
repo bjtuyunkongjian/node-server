f2e8ae41db318ad883df2723d406686e02f87ac6
sl204984 <17798520003@163.com>
2018-11-11 23:14:07 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const shengdaoRouter = require('./routers/shengdao');
+
+// 支持跨域配置
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true'); // 和客户端对应，必须设置以后，才能接收cookie.
+  next();
+};
+
+app.use(allowCrossDomain); // 可以跨域
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// 路由
+app.use('/shengdao', shengdaoRouter);
+
+app.use('/static', express.static('static'));
+
+app.listen(3000);
