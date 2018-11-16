d9ff6cb93d5b014d4e0236e7b0785e6a8849738c
sl204984 <17798520003@163.com>
2018-11-16 11:00:52 +0800

    道路

---
+++ geojson/index.js
@@ -0,0 +1,46 @@
+const gaoguoGDB = require('./gaoguoGDB_50'); // 国道的geojson数据
+const gaoguoGDB_cx = require('./gaoguoGDB_50'); // 抽稀过的国道的geojson数据
+// 9级显示
+const shengGDBt = require('./shengGDBt_50'); // 省道的geojson数据
+const shengGDBt_cx = require('./shengGDBt_5'); // 省道抽稀过后的geojson数据
+// 12级显示
+const zgdGDB = require('./zgdGDB_50'); // 主干道
+const zgdGDB_cx = require('./zgdGDB_5'); // 主干道
+const xiandaoGDB = require('./xiandaoGDB_50'); // 县道
+const xiandaoGDB_cx = require('./xiandaoGDB_5'); // 县道
+const gjlGDB = require('./gjlGDB'); // 高架路
+const gjlGDB_cx = require('./gjlGDB_20'); // 高架路
+// 13级显示
+const xiangdaoGDB = require('./xiangdaoGDB_50'); // 乡道
+const xiangdaoGDB_cx = require('./xiangdaoGDB_5'); // 乡道
+const kslGDB = require('./kslGDB'); // 快速路
+const kslGDB_cx = require('./kslGDB'); // 快速路
+const zadaoGDBt = require('./zadaoGDBt'); // 匝道
+// const zadaoGDBt_cx = require('./zadaoGDBt_cx'); // 匝道
+const cgdGDB = require('./cgdGDB_50'); // 次干道
+const cgdGDB_cx = require('./cgdGDB_5'); // 次干道
+// 14级显示
+const zxGDB = require('./zxGDB_50'); // 支线
+const zxGDB_cx = require('./zxGDB_10'); // 支线
+
+module.exports = {
+  gaoguoGDB,
+  gaoguoGDB_cx,
+  shengGDBt,
+  shengGDBt_cx,
+  zgdGDB,
+  zgdGDB_cx,
+  xiandaoGDB,
+  xiandaoGDB_cx,
+  gjlGDB,
+  gjlGDB_cx,
+  xiangdaoGDB,
+  xiangdaoGDB_cx,
+  kslGDB,
+  kslGDB_cx,
+  zadaoGDBt,
+  cgdGDB,
+  cgdGDB_cx,
+  zxGDB,
+  zxGDB_cx
+};
--- routers/road/index.js
+++ routers/road/index.js
@@ -4,36 +4,13 @@
 const express = require('express');
 const router = express.Router();
 
+const geojson = require('../../geojson');
 const computerFeatures = require('./filter');
 
-const gaoguoGDB = require('../../geojson/gaoguoGDB'); // 国道的geojson数据
-const gaoguoGDB_cx = require('../../geojson/gaoguoGDB_cx'); // 抽稀过的国道的geojson数据
-//////////////////
-// 9级显示
-const shengGDBt = require('../../geojson/shengGDBt'); // 省道的geojson数据
-const shengGDBt_cx = require('../../geojson/shengGDBt_cx'); // 省道抽稀过后的geojson数据
-//////////////////
-// 12级显示
-const zgdGDB = require('../../geojson/zgdGDB'); // 主干道
-const zgdGDB_cx = require('../../geojson/zgdGDB_cx'); // 主干道
-const xiandaoGDB = require('../../geojson/xiandaoGDB'); // 县道
-const xiandaoGDB_cx = require('../../geojson/xiandaoGDB_cx'); // 县道
-const gjlGDB = require('../../geojson/gjlGDB'); // 高架路
-const gjlGDB_cx = require('../../geojson/gjlGDB_cx'); // 高架路
-//////////////////
-// 13级显示
-const xiangdaoGDB = require('../../geojson/xiangdaoGDB'); // 乡道
-const xiangdaoGDB_cx = require('../../geojson/xiangdaoGDB_cx'); // 乡道
-const kslGDB = require('../../geojson/kslGDB'); // 快速路
-const kslGDB_cx = require('../../geojson/kslGDB_cx'); // 快速路
-const zadaoGDBt = require('../../geojson/zadaoGDBt'); // 匝道
-// const zadaoGDBt_cx = require('../../geojson/zadaoGDBt_cx'); // 匝道
-const cgdGDB = require('../../geojson/cgdGDB'); // 次干道
-const cgdGDB_cx = require('../../geojson/cgdGDB_cx'); // 次干道
-//////////////////
-// 14级显示
-const zxGDB = require('../../geojson/zxGDB'); // 支线
-const zxGDB_cx = require('../../geojson/zxGDB_cx'); // 支线
+const emptyRoad = {
+  type: 'FeatureCollection',
+  features: []
+};
 
 router.post('/', (req, res) => {
   const {
@@ -46,140 +23,71 @@ router.post('/', (req, res) => {
   const road = {}; // 所有道路信息数据
   // 国道
   if(zoom >= 15) {
-    road.guodao = computerFeatures(gaoguoGDB, bounds); // 国道
-    road.shengdao = computerFeatures(shengGDBt, bounds); // 省道
-    road.zgd = computerFeatures(zgdGDB, bounds); // 主干道
-    road.xiandao = computerFeatures(xiandaoGDB, bounds); // 县道
-    road.gjl = computerFeatures(gjlGDB, bounds); // 高架路
-    road.xiangdao = computerFeatures(xiangdaoGDB, bounds); // 乡道
-    road.ksl = computerFeatures(kslGDB, bounds); // 快速路
-    road.zadao = computerFeatures(zadaoGDBt, bounds); // 匝道
-    road.cgd = computerFeatures(cgdGDB, bounds); // 次干道
-    road.zx = computerFeatures(zxGDB, bounds); // 支线
+    road.guodao = computerFeatures(geojson.gaoguoGDB, bounds); // 国道
+    road.shengdao = computerFeatures(geojson.shengGDBt, bounds); // 省道
+    road.zgd = computerFeatures(geojson.zgdGDB, bounds); // 主干道
+    road.xiandao = computerFeatures(geojson.xiandaoGDB, bounds); // 县道
+    road.gjl = computerFeatures(geojson.gjlGDB, bounds); // 高架路
+    road.xiangdao = computerFeatures(geojson.xiangdaoGDB, bounds); // 乡道
+    road.ksl = computerFeatures(geojson.kslGDB, bounds); // 快速路
+    road.zadao = computerFeatures(geojson.zadaoGDBt, bounds); // 匝道
+    road.cgd = computerFeatures(geojson.cgdGDB, bounds); // 次干道
+    road.zx = computerFeatures(geojson.zxGDB, bounds); // 支线
   } else if (zoom >= 14) { 
-    road.guodao = computerFeatures(gaoguoGDB_cx, bounds); // 国道
-    road.shengdao = computerFeatures(shengGDBt_cx, bounds); // 省道
-    road.zgd = computerFeatures(zgdGDB_cx, bounds); // 主干道
-    road.xiandao = computerFeatures(xiandaoGDB_cx, bounds); // 县道
-    road.gjl = computerFeatures(gjlGDB_cx, bounds); // 高架路
-    road.xiangdao = computerFeatures(xiangdaoGDB_cx, bounds); // 乡道
-    road.ksl = computerFeatures(kslGDB_cx, bounds); // 快速路
-    road.zadao = computerFeatures(zadaoGDBt, bounds); // 匝道
-    road.cgd = computerFeatures(cgdGDB_cx, bounds); // 次干道
-    road.zx = computerFeatures(zxGDB_cx, bounds); // 支线
+    road.guodao = computerFeatures(geojson.gaoguoGDB_cx, bounds); // 国道
+    road.shengdao = computerFeatures(geojson.shengGDBt_cx, bounds); // 省道
+    road.zgd = computerFeatures(geojson.zgdGDB_cx, bounds); // 主干道
+    road.xiandao = computerFeatures(geojson.xiandaoGDB_cx, bounds); // 县道
+    road.gjl = computerFeatures(geojson.gjlGDB_cx, bounds); // 高架路
+    road.xiangdao = computerFeatures(geojson.xiangdaoGDB_cx, bounds); // 乡道
+    road.ksl = computerFeatures(geojson.kslGDB_cx, bounds); // 快速路
+    road.zadao = computerFeatures(geojson.zadaoGDBt, bounds); // 匝道
+    road.cgd = computerFeatures(geojson.cgdGDB_cx, bounds); // 次干道
+    road.zx = computerFeatures(geojson.zxGDB_cx, bounds); // 支线
   } else if (zoom >= 13) {
-    road.guodao = computerFeatures(gaoguoGDB_cx, bounds); // 国道
-    road.shengdao = computerFeatures(shengGDBt_cx, bounds); // 省道
-    road.zgd = computerFeatures(zgdGDB_cx, bounds); // 主干道
-    road.xiandao = computerFeatures(xiandaoGDB_cx, bounds); // 县道
-    road.gjl = computerFeatures(gjlGDB_cx, bounds); // 高架路
-    road.xiangdao = computerFeatures(xiangdaoGDB_cx, bounds); // 乡道
-    road.ksl = computerFeatures(kslGDB_cx, bounds); // 快速路
-    road.zadao = computerFeatures(zadaoGDBt, bounds); // 匝道
-    road.cgd = computerFeatures(cgdGDB_cx, bounds); // 次干道
-    road.zx = {
-      type: zxGDB_cx.type,
-      features: []
-    }; // 支线
+    road.guodao = computerFeatures(geojson.gaoguoGDB_cx, bounds); // 国道
+    road.shengdao = computerFeatures(geojson.shengGDBt_cx, bounds); // 省道
+    road.zgd = computerFeatures(geojson.zgdGDB_cx, bounds); // 主干道
+    road.xiandao = computerFeatures(geojson.xiandaoGDB_cx, bounds); // 县道
+    road.gjl = computerFeatures(geojson.gjlGDB_cx, bounds); // 高架路
+    road.xiangdao = computerFeatures(geojson.xiangdaoGDB_cx, bounds); // 乡道
+    road.ksl = computerFeatures(geojson.kslGDB_cx, bounds); // 快速路
+    road.zadao = computerFeatures(geojson.zadaoGDBt, bounds); // 匝道
+    road.cgd = computerFeatures(geojson.cgdGDB_cx, bounds); // 次干道
+    road.zx = emptyRoad; // 支线
   }  else if (zoom >= 12) {
-    road.guodao = computerFeatures(gaoguoGDB_cx, bounds); // 国道
-    road.shengdao = computerFeatures(shengGDBt_cx, bounds); // 省道
-    road.zgd = computerFeatures(zgdGDB_cx, bounds); // 主干道
-    road.xiandao = computerFeatures(xiandaoGDB_cx, bounds); // 县道
-    road.gjl = computerFeatures(gjlGDB_cx, bounds); // 高架路
-    road.xiangdao = {
-      type: xiangdaoGDB_cx.type,
-      features: []
-    }; // 乡道
-    road.ksl = {
-      type: kslGDB_cx.type,
-      features: []
-    }; // 快速路
-    road.zadao = {
-      type: zadaoGDBt.type,
-      features: []
-    }; // 匝道
-    road.cgd = {
-      type: cgdGDB_cx.type,
-      features: []
-    }; // 次干道
-    road.zx = {
-      type: zxGDB_cx.type,
-      features: []
-    }; // 支线
+    road.guodao = computerFeatures(geojson.gaoguoGDB_cx, bounds); // 国道
+    road.shengdao = computerFeatures(geojson.shengGDBt_cx, bounds); // 省道
+    road.zgd = computerFeatures(geojson.zgdGDB_cx, bounds); // 主干道
+    road.xiandao = computerFeatures(geojson.xiandaoGDB_cx, bounds); // 县道
+    road.gjl = computerFeatures(geojson.gjlGDB_cx, bounds); // 高架路
+    road.xiangdao = emptyRoad; // 乡道
+    road.ksl = emptyRoad; // 快速路
+    road.zadao = emptyRoad; // 匝道
+    road.cgd = emptyRoad; // 次干道
+    road.zx = emptyRoad; // 支线
   } else if (zoom >= 9) { // 显示抽稀过的国道和省道数据
-    road.guodao = computerFeatures(gaoguoGDB_cx, bounds); // 国道
-    road.shengdao = computerFeatures(shengGDBt_cx, bounds); // 省道
-    road.zgd = {
-      "type": zgdGDB_cx.type,
-      features: []
-    }; // 主干道
-    road.xiandao = {
-      type: xiandaoGDB_cx.type,
-      features: []
-    }; // 县道
-    road.gjl = {
-      type: gjlGDB_cx.type,
-      features: []
-    }; // 高架路
-    road.xiangdao = {
-      type: xiangdaoGDB_cx.type,
-      features: []
-    }; // 乡道
-    road.ksl = {
-      type: kslGDB_cx.type,
-      features: []
-    }; // 快速路
-    road.zadao = {
-      type: zadaoGDBt.type,
-      features: []
-    }; // 匝道
-    road.cgd = {
-      type: cgdGDB_cx.type,
-      features: []
-    }; // 次干道
-    road.zx = {
-      type: zxGDB_cx.type,
-      features: []
-    }; // 支线
+    road.guodao = emptyRoad; // 国道
+    road.shengdao = computerFeatures(geojson.shengGDBt_cx, bounds); // 省道
+    road.zgd = emptyRoad; // 主干道
+    road.xiandao = emptyRoad; // 县道
+    road.gjl = emptyRoad; // 高架路
+    road.xiangdao = emptyRoad; // 乡道
+    road.ksl = emptyRoad; // 快速路
+    road.zadao = emptyRoad; // 匝道
+    road.cgd = emptyRoad; // 次干道
+    road.zx = emptyRoad; // 支线
   } else { // 显示抽稀过的国道数据
-    road.guodao = computerFeatures(gaoguoGDB_cx, bounds); // 国道
-    road.shengdao = {
-      "type": shengGDBt_cx.type,
-      features: []
-    }; // 省道
-    road.zgd = {
-      "type": zgdGDB_cx.type,
-      features: []
-    }; // 主干道
-    road.xiandao = {
-      type: xiandaoGDB_cx.type,
-      features: []
-    }; // 县道
-    road.gjl = {
-      type: gjlGDB_cx.type,
-      features: []
-    }; // 高架路
-    road.xiangdao = {
-      type: xiangdaoGDB_cx.type,
-      features: []
-    }; // 乡道
-    road.ksl = {
-      type: kslGDB_cx.type,
-      features: []
-    }; // 快速路
-    road.zadao = {
-      type: zadaoGDBt.type,
-      features: []
-    }; // 匝道
-    road.cgd = {
-      type: cgdGDB_cx.type,
-      features: []
-    }; // 次干道
-    road.zx = {
-      type: zxGDB_cx.type,
-      features: []
-    }; // 支线
+    road.guodao = emptyRoad; // 国道
+    road.shengdao = emptyRoad; // 省道
+    road.zgd = emptyRoad; // 主干道
+    road.xiandao = emptyRoad; // 县道
+    road.gjl = emptyRoad; // 高架路
+    road.xiangdao = emptyRoad; // 乡道
+    road.ksl = emptyRoad; // 快速路
+    road.zadao = emptyRoad; // 匝道
+    road.cgd = emptyRoad; // 次干道
+    road.zx = emptyRoad; // 支线
   }
 
   const endTime = (new Date()).getTime();
